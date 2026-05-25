1b03947820d1f690cb19883c649f2421b39a7ec9
DUPERLY <duperly77@gmail.com>
2026-05-25 18:14:45 -0500

    Subo modulo preguntasFrecuentes

---
+++ src/app/preguntasFrecuentes/page.jsx
@@ -0,0 +1,17 @@
+import FooterComponent from "../components/FooterComponent";
+import HeaderComponent from "../components/HeaderComponent";
+import Preguntas from "../components/Preguntas";
+
+
+
+export default function Page() {
+  return (
+    <>
+     <HeaderComponent/>
+     <Preguntas/>
+     <FooterComponent/>
+
+
+    </>
+  );
+}
